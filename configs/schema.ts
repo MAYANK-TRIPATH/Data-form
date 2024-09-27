b5701d76d79c1b2407c52e4af8d0d4ee52e195c0
Mayank Tripathi <mr.mayanktripathi2001@gmail.com>
2024-09-27 11:16:27 +0530

    DB setup

---
+++ configs/schema.ts
@@ -0,0 +1,9 @@
+const { pgTable, serial, text, varchar } = require("drizzle-orm/pg-core");
+
+
+export  const JsonForms = pgTable("json_forms", {
+    id:serial("id").primaryKey(),
+    jsonform:text("jsonform").notNull(),
+    createdBy:varchar("createdBy").notNull(),
+    createdAt:varchar("createdAt").notNull(),
+});
